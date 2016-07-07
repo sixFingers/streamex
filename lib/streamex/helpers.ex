1babed6ba0601b53771c59948d7f6519261d790c
sixFingers <ignazio.setti@gmail.com>
2016-07-07 16:13:23 +0200

    Cosmetic newline

--- lib/streamex/helpers.ex
+++ lib/streamex/helpers.ex
@@ -1,6 +1,7 @@
 defmodule Streamex.Helpers do
-  def to_struct(kind, attrs) do
+  def to_struct(attrs, kind) do
     struct = struct(kind)
+
     Enum.reduce Map.to_list(struct), struct, fn {k, _}, acc ->
       case Map.fetch(attrs, Atom.to_string(k)) do
         {:ok, v} -> %{acc | k => v}
