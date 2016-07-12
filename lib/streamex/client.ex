1ea7bfa07708b4b39128e66a37e0f1a706302fb2
sixFingers <ignazio.setti@gmail.com>
2016-07-12 16:09:49 +0200

    First draft of working application-level authentication

--- lib/streamex/client.ex
+++ lib/streamex/client.ex
@@ -1,8 +1,12 @@
 defmodule Streamex.Client do
   use HTTPoison.Base
+  import Timex
+  alias Timex.DateTime, as: DateTime
+  alias Streamex.Request
 
   @api_region Application.get_env(:streamex, :region)
   @api_key Application.get_env(:streamex, :key)
+  @api_secret Application.get_env(:streamex, :secret)
   @api_url "api.getstream.io/api/v1.0"
 
   def jwt_request(url, method, token, body \\ "", params \\ %{}) do
@@ -10,10 +14,32 @@ defmodule Streamex.Client do
       method,
       api_url(url, params),
       body,
-      request_headers(token),
+      jwt_request_headers(token),
       request_options()
     )
     |> parse_response
+  end
+
+  def signed_request(url, method, body \\ "", params \\ %{}) do
+    algoritm = "hmac-sha256"
+    {_, now} = DateTime.local() |> format("{RFC822}")
+    api_key_header = {"X-Api-Key", @api_key}
+    date_header = {"Date", now}
+    content_type_header = {"content-type", "application/json"}
+
+    headers_value = "date"
+    header_field_string = "date: #{now}"
+    signature = :crypto.hmac(:sha256, @api_secret, header_field_string) |> Base.encode64
+    auth_header = {"Authorization", "Signature keyId=\"#{@api_key}\",algorithm=\"#{algoritm}\",headers=\"#{headers_value}\",signature=\"#{signature}\""}
+    headers = [api_key_header, date_header, auth_header, content_type_header]
+
+    request!(
+      method,
+      api_url(url, params),
+      body,
+      headers,
+      request_options()
+    )
   end
 
   defp parse_response({:ok, response}) do
@@ -23,15 +49,15 @@ defmodule Streamex.Client do
 
   defp parse_response({:error, body}), do: {:error, body}
 
-  defp api_url(url, params) when params == [] do
+  defp api_url(url, params) when params == %{} do
     "https://#{@api_region}-#{@api_url}/#{url}?api_key=#{@api_key}"
   end
 
   defp api_url(url, params) do
-    <<api_url(url, []) :: binary, "&", URI.encode_query(params) :: binary>>
+    <<api_url(url, %{}) :: binary, "&", URI.encode_query(params) :: binary>>
   end
 
-  defp request_headers(token) do
+  defp jwt_request_headers(token) do
     [
       {"Authorization", token},
       {"stream-auth-type", "jwt"},
