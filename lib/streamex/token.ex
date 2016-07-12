1d903536b33265238a61ff362e8969fc584753f7
sixFingers <ignazio.setti@gmail.com>
2016-07-12 17:43:10 +0200

    Remove dependencies to parameters

--- lib/streamex/token.ex
+++ lib/streamex/token.ex
@@ -1,12 +1,10 @@
 defmodule Streamex.Token do
   import Joken
 
-  @api_secret Application.get_env(:streamex, :secret)
-
-  def new(%Streamex.Feed{} = feed, resource, action) do
+  def new(secret, %Streamex.Feed{} = feed, resource, action) do
     %{resource: resource, action: action, feed_id: feed.id}
     |> token
-    |> with_signer(hs256(@api_secret))
+    |> with_signer(hs256(secret))
     |> sign
     |> get_compact
   end
