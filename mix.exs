e72eba077a9e16aa73c42cb4ded8cd26654dc026
sixFingers <ignazio.setti@gmail.com>
2016-07-19 01:41:25 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Streamex.Mixfile do
 
   def project do
     [app: :streamex,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
