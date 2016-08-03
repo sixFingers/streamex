c06f1b34599d05d924bbc811576c5e2224555f82
sixFingers <ignazio.setti@gmail.com>
2016-08-03 17:37:19 +0200

    Add generic validation helper

---
+++ lib/streamex/helpers.ex
@@ -0,0 +1,22 @@
+defmodule Streamex.Helpers do
+  def validate([string | t]) do
+    case validate(string) do
+      false -> false
+      true -> validate(t)
+    end
+  end
+
+  def validate([]), do: true
+
+  def validate({source, target} = t) do
+    validate(Tuple.to_list(t))
+  end
+
+  def validate(string) do
+    !Regex.match?(~r/\W/, string)
+  end
+
+  def validate_error do
+    {:error, "Invalid input"}
+  end
+end
