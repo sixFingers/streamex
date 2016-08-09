017b77afee3e165e6f55bdc9bb6898d18dc0f8a9
sixFingers <ignazio.setti@gmail.com>
2016-08-09 18:22:27 +0200

    Cleanup

--- lib/streamex/follow.ex
+++ lib/streamex/follow.ex
@@ -5,17 +5,4 @@ defmodule Streamex.Follow do
             updated_at: nil
 
   @type t :: %__MODULE__{}
-
-  def to_struct(%{} = attrs) do
-    attrs
-    |> Map.to_list
-    |> Enum.reduce(struct(%__MODULE__{}), fn({k, v}, acc) ->
-      %{acc | String.to_atom(k) => v}
-    end)
-  end
-
-  def to_json(%__MODULE__{} = follow) do
-    {:ok, body} = Poison.encode(follow)
-    body
-  end
 end
