2c2f349971b49a957913be42c3afa801b90cd793
sixFingers <ignazio.setti@gmail.com>
2016-08-10 00:27:12 +0200

    Cleanup

--- mix.exs
+++ mix.exs
@@ -19,22 +19,10 @@ defmodule Streamex.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :httpoison, :timex]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:httpoison, "~> 0.9.0"},
@@ -53,11 +41,14 @@ defmodule Streamex.Mixfile do
   end
 
   defp package do
-    [# These are the default files included in the package
-     name: :streamex,
-     maintainers: ["Ignazio Setti"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/sixFingers/streamex",
-              "Docs" => "http://sixfingers.github.io/streamex/"}]
+    [
+      name: :streamex,
+      maintainers: ["Ignazio Setti"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/sixFingers/streamex",
+        "Docs" => "http://sixfingers.github.io/streamex/"
+      }
+    ]
   end
 end
