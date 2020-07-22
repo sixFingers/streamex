8a02eccfa612843020fa31aa0d13cff0de0558a0
Ronald Curtis <roncurtis5868@gmail.com>
2020-07-22 16:37:54 +0900

    chore: mix format and remove comment

--- lib/streamex/client.ex
+++ lib/streamex/client.ex
@@ -13,8 +13,8 @@ defmodule Streamex.Client do
   Returns a `Streamex.Request`.
   """
   def prepare_request(%Request{} = req) do
-    uri = URI.merge(Config.base_url, req.path)
-    query = Map.merge(req.params, %{"api_key" => Config.key}) |> URI.encode_query
+    uri = URI.merge(Config.base_url(), req.path)
+    query = Map.merge(req.params, %{"api_key" => Config.key()}) |> URI.encode_query()
     uri = %{uri | query: query}
     %{req | url: to_string(uri)}
   end
@@ -27,8 +27,8 @@ defmodule Streamex.Client do
   """
   def sign_request(%Request{} = req) do
     case req.token do
-      nil -> sign_request_with_key_secret(req, Config.key, Config.secret)
-      _ -> sign_request_with_token(req, Config.secret)
+      nil -> sign_request_with_key_secret(req, Config.key(), Config.secret())
+      _ -> sign_request_with_token(req, Config.secret())
     end
   end
 
@@ -56,11 +56,12 @@ defmodule Streamex.Client do
   defp sign_request_with_token(%Request{} = req, secret) do
     token = Token.compact(req.token, secret)
 
-    # IO.inspect {:token, token}
-    headers = [
-      {"Authorization", token},
-      {"stream-auth-type", "jwt"},
-    ] ++ req.headers
+    headers =
+      [
+        {"Authorization", token},
+        {"stream-auth-type", "jwt"}
+      ] ++ req.headers
+
     %{req | headers: headers}
   end
 
@@ -72,8 +73,13 @@ defmodule Streamex.Client do
     date_header = {"Date", now}
     headers_value = "date"
     header_field_string = "#{headers_value}: #{now}"
-    signature = :crypto.hmac(:sha256, secret, header_field_string) |> Base.encode64
-    auth_header = {"Authorization", "Signature keyId=\"#{key}\",algorithm=\"#{algoritm}\",headers=\"#{headers_value}\",signature=\"#{signature}\""}
+    signature = :crypto.hmac(:sha256, secret, header_field_string) |> Base.encode64()
+
+    auth_header =
+      {"Authorization",
+       "Signature keyId=\"#{key}\",algorithm=\"#{algoritm}\",headers=\"#{headers_value}\",signature=\"#{
+         signature
+       }\""}
 
     headers = [api_key_header, date_header, auth_header] ++ req.headers
     %{req | headers: headers}
