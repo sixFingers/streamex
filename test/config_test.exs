a2a6115b5709d98a1582a3300703d1344276eeb4
Ronald Curtis <roncurtis5868@gmail.com>
2020-07-22 14:25:36 +0900

    fix: fix config tests

--- test/config_test.exs
+++ test/config_test.exs
@@ -6,11 +6,11 @@ defmodule ConfigTest do
 
   test "Configuration handles empty region" do
     Application.put_env(:streamex, :region, "")
-    assert Config.base_url == "https://api.getstream.io/api/1.0"
+    assert Config.base_url() == "https://api.stream-io-api.com/api/v1.0/"
   end
 
   test "Configuration handles region" do
     Application.put_env(:streamex, :region, "us-west")
-    assert Config.base_url == "https://us-west-api.getstream.io/api/1.0"
+    assert Config.base_url() == "https://us-west-api.stream-io-api.com/api/v1.0/"
   end
 end
