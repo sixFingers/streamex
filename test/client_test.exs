212544fc4ef83c1fc0119d872e8317cf76569099
Ronald Curtis <roncurtis5868@gmail.com>
2020-07-22 14:24:27 +0900

    fix: fix client tests

--- test/client_test.exs
+++ test/client_test.exs
@@ -7,26 +7,30 @@ defmodule ClientTest do
 
   test "client builds requests with correct full url" do
     # region = Application.put_env(:streamex, :region, "us-west")
-    req = %Request{} |> Request.with_params(%{"parameter" => "value"}) |> Client.prepare_request
-    assert req.url == "https://api.getstream.io/api/1.0?api_key=#{Config.key}&parameter=value"
+    req = %Request{} |> Request.with_params(%{"parameter" => "value"}) |> Client.prepare_request()
+
+    assert req.url ==
+             "https://api.stream-io-api.com/api/v1.0/?api_key=#{Config.key()}&parameter=value"
   end
 
   test "client correctly signs request with token" do
     feed = %Streamex.Feed{slug: "user", user_id: "eric", id: "usereric"}
 
-    req = %Request{}
-    |> Request.with_token(feed, "feed", "read")
-    |> Client.sign_request
+    req =
+      %Request{}
+      |> Request.with_token(feed, "feed", "read")
+      |> Client.sign_request()
 
-    headers = req.headers
-    |> Enum.reduce(%{}, fn({k, v}, acc) -> Map.update(acc, k, v, &(&1)) end)
+    headers =
+      req.headers
+      |> Enum.reduce(%{}, fn {k, v}, acc -> Map.update(acc, k, v, & &1) end)
 
     token_string = Map.get(headers, "Authorization", nil)
     assert token_string !== nil
 
     assert Map.get(headers, "stream-auth-type", nil) == "jwt"
-    
-    claims = Streamex.Token.decompact(token_string, Config.secret)
+
+    claims = Streamex.Token.decompact(token_string, Config.secret())
 
     assert Map.get(claims, "resource", nil) == "feed"
     assert Map.get(claims, "action", nil) == "read"
@@ -34,14 +38,18 @@ defmodule ClientTest do
   end
 
   test "client correctly signs request with key/secret" do
-    req = %Request{} |> Client.sign_request
-    headers = req.headers |> Enum.reduce(%{}, fn({k, v}, acc) -> Map.update(acc, k, v, &(&1)) end)
+    req = %Request{} |> Client.sign_request()
+    headers = req.headers |> Enum.reduce(%{}, fn {k, v}, acc -> Map.update(acc, k, v, & &1) end)
     date = Map.get(headers, "Date", "")
     header_field_string = "date: #{date}"
-    signature = :crypto.hmac(:sha256, Config.secret, header_field_string) |> Base.encode64
-    auth_header = "Signature keyId=\"#{Config.key}\",algorithm=\"hmac-sha256\",headers=\"date\",signature=\"#{signature}\""
+    signature = :crypto.hmac(:sha256, Config.secret(), header_field_string) |> Base.encode64()
 
-    assert Map.get(headers, "X-Api-Key", "") == Config.key
+    auth_header =
+      "Signature keyId=\"#{Config.key()}\",algorithm=\"hmac-sha256\",headers=\"date\",signature=\"#{
+        signature
+      }\""
+
+    assert Map.get(headers, "X-Api-Key", "") == Config.key()
     assert Map.get(headers, "Authorization", "") == auth_header
   end
 end
